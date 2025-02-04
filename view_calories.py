106dbc0ef0c61c610d71659d43dd5625f544cde7
SimonaStoeva <s22621604@onlineedu.tu-varna.bg>
2025-02-04 20:27:50 +0200

    Updates view_calories.py

--- view_calories.py
+++ view_calories.py
@@ -2,3 +2,10 @@ def view_calories():
     with open('calorie_tracker.txt', 'r') as file:
         calories = file.read()
         print(calories)
+
+        total_calories = 0
+        with open("calorie_tracker.txt", "r") as file:
+            for line in file:
+                entered_food, entered_consumed_calories = line.strip().split(", ")
+                total_calories += int(entered_consumed_calories)
+        print(f"Total calories: {total_calories}")
